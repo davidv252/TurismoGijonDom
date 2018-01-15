95428bd2ee2eefada4e33da4bf77f47490929abf
davidecdam <david.97.dunas@gmail.com>
2018-01-15 09:35:19 +0100

    Menu contextual y strings para el menú.

--- app/src/main/java/com/example/merche/asociacionesgijondom/MainActivity.java
+++ app/src/main/java/com/example/merche/asociacionesgijondom/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.merche.asociacionesgijondom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -32,5 +33,11 @@ public class MainActivity extends AppCompatActivity {
         int[] to={R.id.tv_nombre,R.id.tv_direccion,R.id.tv_tfno,R.id.tv_fax};
         adaptador=new SimpleAdapter(this,gestora,R.layout.para_list_view,from,to);
         lista.setAdapter(adaptador);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
