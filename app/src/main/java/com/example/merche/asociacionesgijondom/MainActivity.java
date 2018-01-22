bbd43d8c58c07bf3148ee7890654970657afa3c6
davidv252 <davidv25217@gmail.com>
2018-01-22 09:49:18 +0100

    Funcionalidad ListView y comparadora

--- app/src/main/java/com/example/merche/asociacionesgijondom/MainActivity.java
+++ app/src/main/java/com/example/merche/asociacionesgijondom/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.merche.asociacionesgijondom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -10,14 +14,35 @@ import android.widget.SimpleAdapter;
 import com.example.merche.asociacionesgijondom.informacion.DatosViewBinder;
 import com.example.merche.asociacionesgijondom.informacion.Directorio;
 import com.example.merche.asociacionesgijondom.informacion.GestoraDirectorios;
+import com.example.merche.asociacionesgijondom.informacion.Main2Activity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lista ;
+
     private GestoraDirectorios gestora;
     public static SimpleAdapter adaptador;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lista = (ListView)findViewById(R.id.listView);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent = new Intent(view.getContext(), Main2Activity.class);
+
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 
     @Override
@@ -28,9 +53,9 @@ public class MainActivity extends AppCompatActivity {
             StrictMode.setThreadPolicy(policy);
         }*/
 
-        gestora=new GestoraDirectorios();
-        ListView lista=findViewById(R.id.listView);
-        String[] from = Directorio.getClaves();
+        gestora=new GestoraDirectorios();;
+        lista=findViewById(R.id.listView);
+        String[] from= Directorio.getClaves();
         int[] to={R.id.tv_imagen,R.id.tv_nombre, R.id.tv_tipo};
         adaptador=new SimpleAdapter(this,gestora,R.layout.para_list_view,from,to);
         adaptador.setViewBinder(new DatosViewBinder());
@@ -42,4 +67,11 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+
+
+
+
+
 }
