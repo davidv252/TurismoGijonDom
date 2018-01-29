0e9d6b60f8cb70eaf08644ad8a09bc48c2659b50
davidv252 <davidv25217@gmail.com>
2018-01-29 09:35:40 +0100

    Juntando todo

--- app/src/main/java/com/example/merche/asociacionesgijondom/MainActivity.java
+++ app/src/main/java/com/example/merche/asociacionesgijondom/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.merche.asociacionesgijondom;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,12 +13,16 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 
 import com.example.merche.asociacionesgijondom.informacion.DatosViewBinder;
 import com.example.merche.asociacionesgijondom.informacion.Directorio;
 import com.example.merche.asociacionesgijondom.informacion.GestoraDirectorios;
 import com.example.merche.asociacionesgijondom.informacion.Main2Activity;
+import com.example.merche.asociacionesgijondom.informacion.MostrarDatos;
+
+import static com.example.merche.asociacionesgijondom.informacion.Directorio.getClaves;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,11 +31,40 @@ public class MainActivity extends AppCompatActivity {
     private GestoraDirectorios gestora;
     public static SimpleAdapter adaptador;
 
+    /*Notification.Builder caracretisticas;
+    int cuentaDirectorios;
+    int notiId = 001;*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         lista = (ListView)findViewById(R.id.listView);
+
+        /*cuentaDirectorios = lista.getAdapter().getCount();
+
+        caracretisticas =
+                new Notification.Builder(this).
+                        setSmallIcon(android.R.drawable.ic_notification_clear_all).
+                        setContentTitle("¡Aviso!").
+                        setContentText("Se han cargado un total de " + cuentaDirectorios + " directorios");
+
+        Intent resultIntent = new Intent(this, MostrarDatos.class);
+
+        PendingIntent result = PendingIntent.getActivity(
+                this,
+                0,
+                resultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        caracretisticas.setContentIntent(result);
+
+        NotificationManager notMangr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        notMangr.notify(notiId, caracretisticas.build());*/
 
         lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -35,10 +72,14 @@ public class MainActivity extends AppCompatActivity {
 
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-                Intent intent = new Intent(view.getContext(), Main2Activity.class);
+                //Directorio itemSelected = gestora.get(i);
+                //Intent miIntent = new Intent(MainActivity.this, Main2Activity.class);
+                //miIntent.putExtra("obj",itemSelected);
+                //startActivity(miIntent);
 
-                startActivity(intent);
-
+                String[] from = getClaves();
+                int[] to = {R.id.finalImagen, R.id.finalImagen, R.id.finalTelefono, R.id.finalDescripcion, R.id.finalLocalizacion};
+                adaptador = new SimpleAdapter(MainActivity.this, gestora, R.layout.activity_main2, from, to);
             }
 
         });
@@ -53,9 +94,9 @@ public class MainActivity extends AppCompatActivity {
             StrictMode.setThreadPolicy(policy);
         }*/
 
-        gestora=new GestoraDirectorios();;
+        gestora=new GestoraDirectorios();
         lista=findViewById(R.id.listView);
-        String[] from= Directorio.getClaves();
+        String[] from= getClaves();
         int[] to={R.id.tv_imagen,R.id.tv_nombre, R.id.tv_tipo};
         adaptador=new SimpleAdapter(this,gestora,R.layout.para_list_view,from,to);
         adaptador.setViewBinder(new DatosViewBinder());
@@ -67,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
 
 
 
